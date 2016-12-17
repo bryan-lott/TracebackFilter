88e9efdac34b3ee0ff49851d76d4cf006a519e74
Bryan Lott <mystickphoenix@gmail.com>
2016-12-17 13:23:01 -0700

    Make the uberjar much smaller by limiting amazonica.

--- project.clj
+++ project.clj
@@ -4,8 +4,12 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"] ;; logging
                  [org.clojure/tools.cli "0.3.5"] ;; arg parsing
-                 [amazonica "0.3.76"] ;; talk to AWS
+                 [amazonica "0.3.76" :exclusions [com.amaonaws/aws-java-sdk
+                                                  com.amazonaws/amazon-kinesis-client]]  ;; talk to aws, exclude everything
+                 [com.amazonaws/aws-java-sdk-core "1.10.49"] ;; include aws core
+                 [com.amazonaws/aws-java-sdk-sns "1.10.49"] ;; include sns
                  [clj-time "0.13.0"]] ;; timestamps
+                                                                                    
   :target-path "target/%s"
   :main ^:skip-aot tracebackfilter.core
   :profiles {:uberjar {:aot :all
